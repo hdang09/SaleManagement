dbb80b78d20025634a3c847445d51a9fae08d133
atrungbg8285 <atrungbg8285@gmail.com>
2023-11-02 23:33:55 +0700

    add order,checkout

--- SaleManagementWinApp/frmCheckOut.cs
+++ SaleManagementWinApp/frmCheckOut.cs
@@ -11,6 +11,8 @@ namespace SaleManagementWinApp
         public frmCheckOut()
         {
             InitializeComponent();
+            var list = orderDetailDAO.GetAll().Select(p => new { p.OrderId, p.ProductId, p.UnitPrice, p.Quantity, p.Discount }).ToList();
+            dgvListCart.DataSource = list;
         }
 
         private void frmCheckOut_Load(object sender, EventArgs e)
@@ -20,13 +22,19 @@ namespace SaleManagementWinApp
 
         private void dgvListCart_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            
+            if (e.RowIndex >= 0)
+            {
+                var rowSelected = this.dgvListCart.Rows[e.RowIndex];
+
+            }
+            btnCheckout.Enabled = false;
+            btnRemoveCart.Enabled = true;
         }
 
         private void btnRemoveCart_Click(object sender, EventArgs e)
         {
 
-                
+
         }
     }
 }
--- SaleManagementWinApp/frmOrders.cs
+++ SaleManagementWinApp/frmOrders.cs
@@ -10,12 +10,21 @@ namespace SaleManagementWinApp
         public frmOrders()
         {
             InitializeComponent();
-            
+            var list = orderDAO.GetAll().Select(p => new { p.OrderId, p.OrderDate, p.MemberId, p.RequiredDate, p.ShippedDate, p.Freight }).ToList();
+            dgvListOrder.DataSource = list;
         }
 
         private void btnSearchOrder_Click(object sender, System.EventArgs e)
         {
-            
+            var list = orderDAO.GetAll().Where(p => p.OrderDate <= dtpEndDate.Value.Date && p.OrderDate >= dtpStartDate.Value.Date).ToList();
+            if (list != null)
+            {
+                dgvListOrder.DataSource = list;
+            }
+            else
+            {
+                MessageBox.Show("Cannot Find Any Order. Please Try again !", "Notification", MessageBoxButtons.OK);
+            }
         }
     }
 }
